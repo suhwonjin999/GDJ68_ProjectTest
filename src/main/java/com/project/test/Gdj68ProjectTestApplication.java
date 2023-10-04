73d04e619352c6605f21bad175308b80420b6d6e
kimseeun10 <tpdms981@gmail.com>
2023-10-04 16:22:13 +0900

    start project

---
+++ src/main/java/com/project/test/Gdj68ProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj68ProjectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj68ProjectTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/test/Gdj68ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj68ProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
